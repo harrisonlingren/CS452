7f372e1cec36b5e8c37df302b2a2b76e1f36fe2b
Harrison Lingren <harrisonlingren@gmail.com>
2017-04-29 15:33:51 -0400

    finished Node

--- project4/Node.h
+++ project4/Node.h
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <iostream>
 using namespace std;
 
 class Node {
@@ -18,23 +19,21 @@ private:
 
     // REDO THIS - sort children to balance all nodes
     void sort_children() {
-        Node* t;
+        /*if (count_children == 0) {
+            return;
+        }*/
+
+        Node* t = new Node();
         for (int x = 0; x < count_children; x++) {
-            for (int y = 0; y < count_children; y++) {
-                if (child[y]->get_value() > child[y+1]->get_value()) {
+            for (int y = 0; y < count_children - 1; y++) {
+                Node* n1 = child[y]; // cout << "sortNode1: "; n1->print();
+                Node* n2 = child[y+1]; // cout << "sortNode2: "; n2->print();
+                if (n1->get_value() > n2->get_value()) {
                     t = child[y];
                     child[y] = child[y+1];
                     child[y+1] = t;
                 }
             }
-        }
-    }
-
-    // calculate new values for children
-    void calc_child_values() {
-        for (int x = 0; x < 6; x++) {
-            if (x < count_children) { calc_value(x); }
-            else { children_values[x] = -1; }
         }
     }
 
@@ -44,6 +43,11 @@ private:
 
 
 public:
+    // init all attributes
+    int value;
+    static const int left = 0; static const int right = 0;
+    static const int middle = 0; static const int overflow = 0;
+
     // constructor (no val)
     Node() { init(); }
 
@@ -53,19 +57,11 @@ public:
         new_leaf();
     }
 
-
     // is this a leaf...?
     bool is_leaf() {
-        if (count_children == 0) { return true; }
+        if (value == -1) { return false; }
         else { return true; }
     }
-
-
-    // init all attributes
-    int value;
-    static const int left = 0; static const int right = 0;
-    static const int middle = 0; static const int overflow = 0;
-
 
     // init parent & children leaves
     Node* parent; Node* child[6];
@@ -92,6 +88,10 @@ public:
         if (has_middle()) { return child[2]; }
         else if (has_right()) { return child[1]; }
         else { return NULL; }
+    }
+
+    Node* get_parent() {
+        if (1==1) {return parent;}
     }
 
 
@@ -126,22 +126,64 @@ public:
         }
     }
 
+    // print node
+    void print() {
+        if (is_leaf()) {
+            cout << value << " ";
+            return;
+        }
+
+        for (int x = 0; x < count_children; x++) {
+            cout << children_values[x] << " ";
+        }
+    }
 
     // add child
     void add_child(Node* node) {
         child[count_children++] = node;
         value = -1;
         node->set_parent(this);
-        sort_children(); calc_child_values();
+        sort_children();
+        calc_child_values();
     }
 
     // remove child
     Node* remove_child(int pos) {
-        return NULL;
+        Node* removed = child[pos];
+        count_children--;
+
+        for (int x = pos; x < count_children; ++x) {
+            child[pos] = child[pos+1];
+            children_values[pos] == children_values[pos+1];
+        }
+
+        child[count_children] == NULL;
+        children_values[count_children] = -1;
+        return removed;
     }
 
     // shift children
     void shift_children(int oldC, int newC) {
+        if (oldC < newC) {
+            child[newC]->add_child(child[oldC]->remove_child(child[oldC]->count_children-1));
+        } else {
+            child[newC]->add_child(child[oldC]->remove_child(0));
+        }
+    }
 
+    // calculate new values for children
+    void calc_child_values() {
+        for (int x = 0; x < 6; x++) {
+            if (x < count_children) { calc_value(x); }
+            else { children_values[x] = -1; }
+        }
+    }
+
+    void propagate_children() {
+        int oldVal = get_value();
+        calc_child_values();
+        if (oldVal != get_value() && parent != NULL) {
+            parent->propagate_children();
+        }
     }
 };
